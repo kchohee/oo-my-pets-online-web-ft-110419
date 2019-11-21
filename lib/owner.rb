483032604d119c0d8696817e2420a1dcd22d30fc
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-21 00:59:13 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,26 @@
 class Owner
-  # code goes here
+  attr_accessor
+  attr_writer
+  attr_reader :name, :species
+  @@owners = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @owner = self
+    @@owners << self
+    pets = []
+  end
+  def say_species
+    puts "I am #{@species}."
+  end
+  def self.all
+    @@owners
+  end
+  def self.count
+    @@owner.all.count
+  end
+  def self.reset_all
+    @@owners.all.clear
+  end
+  
 end
